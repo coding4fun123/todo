2c84d6451306a92f90f6bb3254133e2d65f1ccb5
ruan060606 <ruan060606@yahoo.com>
2022-08-29 21:09:48 -0700

    heroku

--- index.js
+++ index.js
@@ -10,6 +10,16 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use("/api/todo/", apiRoute)
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+  
+    const path = require('path');
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}`)
 })
